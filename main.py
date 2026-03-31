13c1a24c4c195143d81f695201b42da905251112
Lee byungchul <leech2033@gmail.com>
2026-03-31 13:59:32 +0900

    Add btn2(Clear) and handler function

--- main.py
+++ main.py
@@ -1,38 +1,52 @@
-# ch 4,2,4 main.py
+# ch 4,4,1 main.py
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton,QVBoxLayout,QMessageBox, QPlainTextEdit) # QPlainTextEdit 추가
+from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
+                             QMessageBox, QPlainTextEdit, QHBoxLayout)
 from PyQt5.QtGui import QIcon
 
+
 class Calculator(QWidget):
-
+    
     def __init__(self):
-        super() .__init__()
+        super().__init__()
         self.initUI()
-
+        
     def initUI(self):
-        self.tel = QPlainTextEdit() # 텍스트 에디트 위젯 생성
-        self.tel.setReadOnly(True) # 텍스트 에디트 위젯을 읽기만 가능하도록 수정
-
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
+        
         self.btn1=QPushButton('Message',self)
         self.btn1.clicked.connect(self.activateMessage)
-
+        
+        self.btn2=QPushButton('Clear',self)
+        self.btn2.clicked.connect(self.clearMessage)
+        
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+        
         vbox=QVBoxLayout()
-        vbox.addWidget(self.tel) # 수직 레이아웃에 텍스트 에디트 위젯 추가
-        vbox.addWidget(self.btn1)
+        vbox.addWidget(self.te1)
+        vbox.addLayout(hbox)
         vbox.addStretch(1)
-
-        self.setLayout(vbox) # 빈 공간 - 버튼 - 빈 공간 순으로 수직 배치된 레이아웃 설정
-
-        self.setWindowTitle('Calcuator')
-        self.setWindowIcon(QIcon('icon.png')) # 윈도 아이콘 추가
+        
+        self.setLayout(vbox)
+        
+        self.setWindowTitle('Calculator')
+        self.setWindowIcon(QIcon('icon.png'))
         self.resize(256,256)
         self.show()
-
-    def activateMessage(self): # 핸들러 함수 수정 : 메시지가 텍스트 에디트에 출력되도록
-        # QMessageBox.information(self,"information","Button clicked!")
-        self.tel.appendPlainText("Button clicked!")
-
-if __name__ =='__main__':
+        
+    def activateMessage(self):
+        # QMessageBox.information(self, "information", "Button clicked!")
+        self.te1.appendPlainText("Button clicked!")
+        
+    def clearMessage(self):
+        self.te1.clear()
+        
+        
+if __name__ == '__main__':
     app=QApplication(sys.argv)
     view=Calculator()
     sys.exit(app.exec_())
